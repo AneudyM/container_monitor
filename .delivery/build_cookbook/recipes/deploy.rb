efd5a5c6dfb1158b08d92273432d3f8fa98225ac
Aneudy Mota <aneudy.motacatalino@gmail.com>
2016-09-15 17:04:59 -0400

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/recipes/deploy.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: build_cookbook
+# Recipe:: deploy
+#
+# Copyright (c) 2016 Aneudy Mota, All Rights Reserved.
+include_recipe 'delivery-truck::deploy'
---
+++ .delivery/build_cookbook/recipes/smoke.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: build_cookbook
+# Recipe:: smoke
+#
+# Copyright (c) 2016 Aneudy Mota, All Rights Reserved.
+include_recipe 'delivery-truck::smoke'
